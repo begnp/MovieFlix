c2a6aa7c8354cde8f709dcd17c608ad0d930bd99
Bernardo <bernardoguimaraesnp@gmail.com>
2026-01-07 22:48:27 -0300

    feat: Cria repository de entidade Category

--- src/main/java/com/movieflix/movieflix/repository/CategoryRepository.java
+++ src/main/java/com/movieflix/movieflix/repository/CategoryRepository.java
@@ -1,4 +1,9 @@
 package com.movieflix.movieflix.repository;
 
-public interface CategoryRepository {
+import com.movieflix.movieflix.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository // Annotation não obrigatoria, por não atrapalhar o funcionamento
+public interface CategoryRepository extends JpaRepository<Category, Long> {
 }
