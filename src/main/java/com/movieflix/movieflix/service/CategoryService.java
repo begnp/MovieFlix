22aba34bec76336a49048bfd1f8f220a3620dd56
Bernardo <bernardoguimaraesnp@gmail.com>
2026-01-07 23:47:28 -0300

    feat: Inclui a rota para ver todas as categorias com o método GET e adiciona a annotation @RequiredArgsConstructor

--- src/main/java/com/movieflix/movieflix/service/CategoryService.java
+++ src/main/java/com/movieflix/movieflix/service/CategoryService.java
@@ -1,10 +1,20 @@
 package com.movieflix.movieflix.service;
 
+import com.movieflix.movieflix.entity.Category;
+import com.movieflix.movieflix.repository.CategoryRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class CategoryService {
 
+    private final CategoryRepository repository;
 
+    public List<Category> findAll() {
+        return repository.findAll();
+    }
 
 }
