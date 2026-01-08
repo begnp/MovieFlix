bf8477d560ebef0d87132a7c47209f5e79283ad8
Bernardo <bernardoguimaraesnp@gmail.com>
2026-01-07 22:47:29 -0300

    feat: Cria a entidade Category

---
+++ src/main/java/com/movieflix/movieflix/entity/Category.java
@@ -0,0 +1,19 @@
+package com.movieflix.movieflix.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Entity
+@Table(name = "category")
+@Getter
+@Setter
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 100, nullable = false)
+    private String name;
+
+}
---
+++ src/main/java/com/movieflix/movieflix/repository/CategoryRepository.java
@@ -0,0 +1,4 @@
+package com.movieflix.movieflix.repository;
+
+public interface CategoryRepository {
+}
