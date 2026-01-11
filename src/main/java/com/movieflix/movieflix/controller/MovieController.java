9fc337a6f523f1976c9fc9e2ff4149f4a024bca6
Bernardo <bernardoguimaraesnp@gmail.com>
2026-01-11 00:53:25 -0300

    feat: Inclui endpoint para listar todos em movie e implementa, parcialmente, a rota para criar novo movie

--- src/main/java/com/movieflix/movieflix/controller/MovieController.java
+++ src/main/java/com/movieflix/movieflix/controller/MovieController.java
@@ -1,13 +1,16 @@
 package com.movieflix.movieflix.controller;
 
+import com.movieflix.movieflix.entity.Movie;
+import com.movieflix.movieflix.mapper.MovieMapper;
 import com.movieflix.movieflix.request.MovieRequest;
 import com.movieflix.movieflix.response.MovieResponse;
 import com.movieflix.movieflix.service.MovieService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/movieflix/movie")
@@ -17,8 +20,20 @@ public class MovieController {
     private final MovieService movieService;
 
     @PostMapping
-    public MovieResponse saveMovie(@RequestBody MovieRequest movieRequest) {
-        movieService.save();
+    public ResponseEntity<MovieResponse> saveMovie(@RequestBody MovieRequest movieRequest) {
+        Movie newMovie = movieService.save(MovieMapper.toMovie(movieRequest));
+        MovieResponse movie = MovieMapper.toMovieResponse(newMovie);
+        return ResponseEntity.status(HttpStatus.CREATED).body(movie);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<MovieResponse>> getAllMovies() {
+        List<Movie> movies = movieService.findAll();
+        List<MovieResponse> movieResponses = movies.stream()
+                .map(movie -> MovieMapper.toMovieResponse(movie))
+                //.map(MovieMapper::toMovieResponse)
+                .toList();
+        return ResponseEntity.ok(movieResponses);
     }
 
 }
